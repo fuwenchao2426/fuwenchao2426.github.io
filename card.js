e3128b2fa25be96f43725a139c2961920fb8e0d1
fuwenchao2426 <fuwenchao2426@163.com>
2022-08-20 13:20:15 +0800

    add img

--- card.js
+++ card.js
@@ -15,7 +15,7 @@ $.getJSON("/data.json", function (d) {
             <div class="fcard container shadow-sm overflow-hidden p-0">
                 <div class="col fheader bg-warning text-center">${fname}</div>
                 <div class="row h-100 p-3 bg-primary bg-opacity-50 text-white">
-                    <div class="col-4"><img src="${fimg}" alt="" class="pic bg-success w-100"></div>
+                    <div class="col-4"><img src="${fimg}" alt="" class="pic img-fluid bg-white"></div>
                     <div class="col-8">${ftext}</div>
                 </div>
             </div>
