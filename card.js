6140d768d54c909a0a88c4f55de5c98fd7027213
Fly <fuwenchao2426@163.com>
2022-08-26 16:06:46 +0800

    add tempcolor

--- card.js
+++ card.js
@@ -11,7 +11,7 @@ $.getJSON("/data.json", function (d) {
         ftext = val.text;
         mb = `
         <a class="col-12 col-sm-6 col-lg-4 col-xxl-3 mb-3 text-decoration-none" href="${furl}">
-            <div class="card text-dark bg-info overflow-hidden" style="border-radius: 1rem;">
+            <div class="card text-dark bg-info overflow-hidden shadow" style="border-radius: 1rem;">
                 <h5 class="card-header bg-warning text-center fw-bolder">${fname}</h5>
                 <img src="${fimg}" class="card-img-top p-2" alt="img" style="height: 9rem;">
                 <div class="card-body" style="height: 6rem;>
