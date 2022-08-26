fba33d37fe4ec841b60691de126b77d9c2908d56
Fly <fuwenchao2426@163.com>
2022-08-26 19:51:28 +0800

    new fcolor

--- card.js
+++ card.js
@@ -11,8 +11,8 @@ $.getJSON("/data.json", function (d) {
         ftext = val.text;
         mb = `
         <a class="col-12 col-sm-6 col-lg-4 col-xxl-3 mb-3 text-decoration-none" href="${furl}">
-            <div class="card text-dark bg-info overflow-hidden shadow" style="border-radius: 1rem;">
-                <h5 class="card-header bg-warning text-center fw-bolder">${fname}</h5>
+            <div class="card text-dark bg-info1 bg-dark overflow-hidden shadow" style="border-radius: 1rem;">
+                <h5 class="card-header bg-yellow-1 text-center fw-bolder">${fname}</h5>
                 <img src="${fimg}" class="card-img-top p-2" alt="img" style="height: 10rem;">
                 <div class="card-body" style="height: 6rem;>
                     <p class="card-text">${ftext}</p>
